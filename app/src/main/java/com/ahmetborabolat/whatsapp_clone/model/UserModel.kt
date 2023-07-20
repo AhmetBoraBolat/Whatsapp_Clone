93669ac6e1c06d1c95438dda1f01e735e7f4c822
Ahmet Bora Bolat <borabolat2015@gmail.com>
2023-07-21 01:47:51 +0300

    firebase database

---
+++ app/src/main/java/com/ahmetborabolat/whatsapp_clone/model/UserModel.kt
@@ -0,0 +1,11 @@
+package com.ahmetborabolat.whatsapp_clone.model
+
+
+
+data class UserModel(
+    val uid : String,
+    val name : String,
+    val number : String,
+    val imageUrl : String
+
+)
